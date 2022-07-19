596ff422bbec92b57da94ff104b37b54bff79ecf
victor <jv.paixao.762.g@gmail.com>
2022-07-18 23:33:29 -0300

    modifcação01

---
+++ script.js
@@ -0,0 +1,53 @@
+// PEDINDO UM NOME DE USUÁRIO 
+// prompt('qual seu nome)
+
+// imprimindo meu nome pro usuário
+// console.log('victor')
+
+// atribuindo um nome a uma variável
+let nome = 'victor';
+// imprimindo a variável 
+console.log(nome);
+
+
+let idade = 20;
+console.log(idade);
+
+console.log('olá', 'meu nome é', nome, 'eu tenho', idade, 'anos') 
+
+
+const tenhoPet = true
+let nomeDoPet = 'xereca'
+
+// 
+
+const VictorTemPets = false 
+let nomeDoPetDeVictor = null
+
+// const dinheiro
+//  console.log(typeof dinheiro)
+
+const dinheiro = 50.559
+console.log( typeof dinheiro)
+
+const nomeString = prompt('qual seu nome?');
+const idadeNumber  = prompt('qual sua idade?');
+
+//  tudo oq vem pelo prompt é uma string
+
+// console.log com o type de cada variavel
+// console.logo formando uma frase com as 3 variaveis 
+
+const nomeDoDono ='Milico'
+const idadeDoDono = 29
+const eleRouba = false 
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof eleRouba)
+
+
+
+
+
+
